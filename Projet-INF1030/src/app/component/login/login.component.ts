ffc95cdffbe7913975ee8ddc4d8e125317ec4fdd
duboisnp <duboisnp@uqtr.ca>
2021-01-27 22:11:17 -0500

    Début du Html et typescript du login

--- Projet-INF1030/src/app/component/login/login.component.ts
+++ Projet-INF1030/src/app/component/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  loginInvalid: boolean;
+  formSubmitAttempt: boolean;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
+
+  onSubmit(): void {
+  }
+
+  private initForm() {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.email],
+      password: ['', Validators.required]
+    });
+  }
+
 
 }
