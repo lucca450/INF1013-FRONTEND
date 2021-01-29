19a61947b987b34318600e6e47c89836203e6891
silvalu <silvalu@uqtr.ca>
2021-01-28 22:29:02 -0500

    semble etre mieux niveau responsive, quand passe en mobile ca change le layout du tableau pour etre mieux adapté

--- Projet-INF1030/src/app/services/mock-data.service.ts
+++ Projet-INF1030/src/app/services/mock-data.service.ts
@@ -12,7 +12,7 @@ export class MockDataService {
 
   private mockDoctorData(): Doctor[]{
     return [
-      {lname: 'moumou', fname: 'dau', email: 'test1@gmail.ca', fax: 1111111111, phone: 1111111111},
+      {lname: 'moumoua', fname: 'dau', email: 'test1@gmail.ca', fax: 1111111111, phone: 1111111111},
       {lname: 'doudou', fname: 'aby', email: 'test2@gmail.ca', fax: 2222222222, phone: 2222222222},
       {lname: 'prot', fname: 'col', email: 'test3@gmail.ca', fax: 3333333333, phone: 3333333333}
     ];
