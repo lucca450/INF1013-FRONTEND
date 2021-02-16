cbdbcc1a4b80122efeb17e0e9991c220e3f2bdc4
duboisnp <duboisnp@uqtr.ca>
2021-02-16 18:06:36 -0500

    commit obligatoire

---
+++ Projet-INF1030/src/app/pipes/convert-interface-name.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { ConvertInterfaceNamePipe } from './convert-interface-name.pipe';
+
+describe('ConvertInterfaceNamePipe', () => {
+  it('create an instance', () => {
+    const pipe = new ConvertInterfaceNamePipe();
+    expect(pipe).toBeTruthy();
+  });
+});
