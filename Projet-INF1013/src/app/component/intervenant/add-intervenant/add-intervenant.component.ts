f216e966923db99190fe1875abdfa458e489677d
lucca possa silva <luccapossa450@gmail.com>
2021-03-28 15:02:42 -0400

    ajout des validations pour les champs dans dans add-intervenant + ajout des asterisques pour les champs obligatories dans meeting.

--- Projet-INF1013/src/app/component/intervenant/add-intervenant/add-intervenant.component.ts
+++ Projet-INF1013/src/app/component/intervenant/add-intervenant/add-intervenant.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {IntervenantService} from '../../../services/intervenant/intervenant.service';
-import {Subscription} from "rxjs";
+import {Subscription} from 'rxjs';
 import {Role} from '../../../enum/role.enum';
 
 @Component({
@@ -16,8 +16,8 @@ export class AddIntervenantComponent implements OnInit {
   roleEnum = Object.entries(Role).filter(e => !isNaN(e[0]as any)).map(e => ({ name: e[1], id: e[0] }));
   errorsSubscription: Subscription;
   errorMessage: String;
-  defaultRole: String = "I";
-  defaultActiveInnactif : String = "true";
+  defaultRole: String = 'I';
+  defaultActiveInnactif: String = 'true';
   hide = true;
 
   constructor(private formBuilder: FormBuilder, private intervenantService: IntervenantService) { }
@@ -29,7 +29,7 @@ export class AddIntervenantComponent implements OnInit {
       (error: any) => {
         this.errorMessage = error;
       }
-    )
+    );
   }
 
   onSubmit(): void {
@@ -38,30 +38,31 @@ export class AddIntervenantComponent implements OnInit {
   private initForm(): void {
 
     this.addintervenantForm = this.formBuilder.group({
-      interfaceName:'Intervenant',
-      fname: ['', Validators.required],
-      lname: ['', Validators.required],
+      interfaceName: 'Intervenant',
+      fname: ['', [Validators.required, Validators.maxLength(30)]],
+      lname: ['', [Validators.required, Validators.maxLength(30)]],
       email: ['', [Validators.required, Validators.email]],
       phone: ['', [Validators.required, Validators.pattern('[0-9]{10}')]],
-      address: ['', Validators.required]
+      address: ['', [Validators.required, Validators.maxLength(50)]]
     });
 
     this.addUserForm = this.formBuilder.group({
-      interfaceName:'User',
-      username: ['', Validators.required],
-      password: ['', Validators.required],
+      interfaceName: 'User',
+      username: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(40)]],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(40)]],
       role: ['', Validators.required],
       active: [true, Validators.required]
     });
+
   }
 
   // Fonction pour réagir lorsque la personne clique sur le bouton "Ajouter"
   onAddIntervenant(): void {
-    let element: HTMLElement = document.getElementById('buttonintervenant') as HTMLElement;
+    const element: HTMLElement = document.getElementById('buttonintervenant') as HTMLElement;
     element.click();
 
     if (this.addintervenantForm.valid && this.addUserForm.valid) {
-      this.intervenantService.addIntervenant(this.addintervenantForm.value,this.addUserForm.value);
+      this.intervenantService.addIntervenant(this.addintervenantForm.value, this.addUserForm.value);
     }else {
       alert('Veuillez remplir tous les champs');
     }
