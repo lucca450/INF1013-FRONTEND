f8f90660465cf8832f5f470aa544cc5a96f206ee
lucca possa silva <luccapossa450@gmail.com>
2021-03-28 16:57:21 -0400

    ajout des validations pour les champs dans ADD MEETING + ajout des validations pour les champs dans EDIT MEETING + correction d'un mini probleme lors du edit, j'avais oubli/ de unsubscribe une fois que le edit retourne \ la page précédente.

--- Projet-INF1013/src/app/component/meeting/edit-meeting/edit-meeting.component.ts
+++ Projet-INF1013/src/app/component/meeting/edit-meeting/edit-meeting.component.ts
@@ -94,12 +94,11 @@ export class EditMeetingComponent implements OnInit, OnDestroy {
   }
 
   private initForm(): void {
-    console.log(this.meeting);
     // this.meeting = this.meetingService.getMeetingsFromID(this.meetingID);
     this.editMeetingForm = this.formBuilder.group({
-      notes: [this.meeting.notes, Validators.required],
-      followup: [this.meeting.followup, Validators.required],
-      goals: [this.meeting.goals, Validators.required],
+      notes: [this.meeting.notes, [Validators.required, Validators.minLength(5), Validators.maxLength(4000)]],
+      followup: [this.meeting.followup, [Validators.required, Validators.minLength(5), Validators.maxLength(4000)]],
+      goals: [this.meeting.goals, [Validators.required, Validators.minLength(5), Validators.maxLength(4000)]],
       idPerson: [this.meeting.idPerson, Validators.required],
       idIntervenant: [this.meeting.idIntervenant, Validators.required],
       id: [this.meeting.id]
@@ -114,7 +113,10 @@ export class EditMeetingComponent implements OnInit, OnDestroy {
     if (this.editMeetingForm.valid) {
       console.log(this.editMeetingForm.value);
       this.meetingService.editMeeting(this.editMeetingForm.value);
-
+      this.meetingSubscription.unsubscribe();
+      this.errorsSubscription.unsubscribe();
+      this.intervenantSubscription.unsubscribe();
+      this.personsSubscription.unsubscribe();
     }else {
       alert('Veuillez remplir tous les champs');
     }
