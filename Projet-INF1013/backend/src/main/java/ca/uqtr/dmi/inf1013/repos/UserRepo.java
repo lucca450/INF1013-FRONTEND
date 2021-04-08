0b494c72c933c3f0aba410b4883157c3cd3b39c9
Pierre-Olivier Dubois-Nadeau <duboisnp@uqtr.ca>
2021-04-08 15:13:25 -0400

    Début login

--- Projet-INF1013/backend/src/main/java/ca/uqtr/dmi/inf1013/repos/UserRepo.java
+++ Projet-INF1013/backend/src/main/java/ca/uqtr/dmi/inf1013/repos/UserRepo.java
@@ -4,7 +4,11 @@ import ca.uqtr.dmi.inf1013.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface UserRepo extends CrudRepository<User,Long> { // Va récupérer le champs id dans users en lui mentionnant le type (Doit etre le first)
+public interface UserRepo extends CrudRepository<User,Long> {
+
+  Optional<User> findByUsername(String username); // Va récupérer le champs id dans users en lui mentionnant le type (Doit etre le first)
 
 }
